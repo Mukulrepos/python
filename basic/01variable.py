38bf9a29203a6036f7621862986dc0a213713f80
[Mukulrepos] <[mv282301@gmail.com]>
2024-03-01 14:24:50 +0530

    python

---
+++ basic/01variable.py
@@ -0,0 +1,13 @@
+#
+
+# basic variable start the new thinking time
+chutiya="1 khokha"# variable is chutiya and assign the value using the = operator to 1 khokha
+
+print(len(chutiya))# print the lenght of chutiya varibale can store the variable
+
+
+pyar = "mukul"# meaning full name 
+pyar_dusra= "ma or  baap"# full meaning full name 
+
+
+
---
+++ basic/variabletest.py
@@ -0,0 +1,11 @@
+# frist_number = (int(input("enter the number")))
+# second_number = (int(input("enter the number")))
+# print(frist_number+second_number);
+
+# type conversion
+
+lenght = len("mukul vats")
+print("hello ",lenght)
+
+print(str(lenght))
+print(type (str(lenght)))
