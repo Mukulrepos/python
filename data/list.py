1b2e706da24d5a2b1d31607c3e3949c4ff83d3b1
Mukulrepos <mv282301@gmail.com>
2024-03-15 02:10:57 +0530

    hi

---
+++ data/list.py
@@ -0,0 +1,21 @@
+marks = [1, 2, 4, 5,7, 6]
+print(marks)
+print(marks[3])
+print(marks.append(544))  # add the at end of the array list
+print(marks) 
+print(marks[len(marks) - 4])  # it simply to 5 -4 = 1 index and value is 2
+
+
+# searh the elemets or not 
+if 7 in marks:
+    print("yes")
+else:
+    print("no")
+    
+    
+print(marks[1:6:2])
+
+
+
+
+
