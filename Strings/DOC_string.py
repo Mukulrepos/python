20c35ae09419018284b59ee5f23bd8294f6915fd
Mukulrepos <mv282301@gmail.com>
2024-03-27 01:29:34 +0530

    complete the 30 videos

---
+++ Strings/DOC_string.py
@@ -0,0 +1,6 @@
+def adyn(n):
+    '''takes the numebr n return the square of n '''
+    print(n**2)
+print(adyn.__doc__)
+
+# it  can give the brief line to tell about the fuctions and working principles
---
+++ Strings/f_strings.py
@@ -0,0 +1,7 @@
+
+name = str(input("Enter the name"))
+sir = str(input("Enter the sir"))
+letter = f" hello bhai kasa hai {name}{sir}"
+print(letter.format(name,sir))
+
+
