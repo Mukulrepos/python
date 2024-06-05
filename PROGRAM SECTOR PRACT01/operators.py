cfce3220810c781c145c9e46b8d9ad5edefbe9f1
Mukulrepos <mv282301@gmail.com>
2024-06-05 23:10:51 +0530

    day 1 complete

--- PROGRAM SECTOR PRACT01/operators.py
+++ PROGRAM SECTOR PRACT01/operators.py
@@ -1,17 +1,76 @@
 # Addition
-identity =1 
-identity2=9
-print(identity+identity2)
+# identity =1 
+# identity2=9
+# print(identity+identity2)
 
 
-# logical
-car_loan_amount=int(input("enter your car loan amount:"))
-car_loan_for_age=int(input("enter your age:"))
-if car_loan_for_age>=18 and car_loan_amount>=20000:
-    print("your are eligible")
+# # logical
+# car_loan_amount=int(input("enter your car loan amount:"))
+# car_loan_for_age=int(input("enter your age:"))
+# if car_loan_for_age>=18 and car_loan_amount>=20000:
+#     print("your are eligible")
+# else:
+#     print("your are not eligible for loan")
+#     print("phele bada toh ho ja")
+
+
+# comparison operator
+# a=((4>6)==False)
+# print(a)
+
+# bitwise operator
+# bitwise and
+# a b output
+# 0 0  0
+# 0 1  0
+# 1 0  0
+# 1 1  1
+
+
+#  bit wise  (or)
+#   A   B  output
+#   0   0   0
+#   1   0   1
+#   0   1   1
+#   1   1   1
+
+
+# bitwise XOR
+#   A   B  output
+#   0   0   0
+#   1   0   1
+#   0   1   1
+#   1   1   0
+
+
+# bit wise not
+
+# a  output
+# 1    0
+# 0    1
+
+# identity operator
+# value is equal but house naem is differnent 
+# x = 10 
+# y = 10 
+# print(x is y )
+
+# x = 20 
+# y = 10
+# print(x is not y )
+# o = "20" in x
+# print(x)
+
+
+#  membership oerator 
+assitant=("vision", "chiper", "dark vision ", "operators","opticus")
+assitant_name=str(input("Enter your favourate AI assitant name:->"))
+if assitant_name in assitant:
+    print("yess it have i can call it")
+    print("hi dad i "+assitant_name)
 else:
-    print("your are not eligible for loan")
-    print("phele bada toh ho ja")
-    
-    
-    
+    print("who are ??")
+    print("where to find this database sector ")
+    print("Dark vision check the IP ADDRESS")
+
+
---
+++ PROGRAM SECTOR PRACT01/string.py
@@ -0,0 +1,2 @@
+robot_name=str(input("enter your AI ASSISTANT Computer name:->"))
+print("Your Robot Computer Name is :->",robot_name)
