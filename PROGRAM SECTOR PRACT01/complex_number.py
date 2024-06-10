ce87cb0d57751e0f1a17331e22aa73d1ff2d9df1
Mukulrepos <mv282301@gmail.com>
2024-06-10 21:55:13 +0530

    today work

---
+++ PROGRAM SECTOR PRACT01/complex_number.py
@@ -0,0 +1,28 @@
+# Date_sheet=complex(3+2)
+# Date_sheet2=complex(7+8)
+# Compiles=Date_sheet+Date_sheet2
+# print("ADDITION ON COMPLEX NUMBER",Compiles)
+# convert=int(Date_sheet)
+# print(int(Date_sheet2))
+
+
+
+elements=[10,20,0,40,50,]
+
+create= bytes(elements)
+# create[2]=100
+# print(create[1])
+for i in create:
+    print(i)
+ 
+x = bytearray(elements)
+print(x)
+
+arr=[1,2]
+print(type(arr))
+
+
+lists=[1,1]
+tup=(1,1)
+print(lists)
+print(tup)
--- PROGRAM SECTOR PRACT01/string.py
+++ PROGRAM SECTOR PRACT01/string.py
@@ -1,2 +1,14 @@
-robot_name=str(input("enter your AI ASSISTANT Computer name:->"))
-print("Your Robot Computer Name is :->",robot_name)
+# robot_name=str(input("enter your AI ASSISTANT Computer name:->"))
+# print("Your Robot Computer Name is :->",robot_name)
+robot= "Vision"
+print("hello "+robot)
+
+
+comand_message="""vision hi
+You can use the AWA KEY to come here please 
+but don't ask anything  
+this is my command 
+unfollow all comand follow only my command portal 
+come fast
+"""
+print(comand_message)
